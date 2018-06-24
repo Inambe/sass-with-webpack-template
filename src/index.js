74edb4728bf607367821a97b7dd16b64bb80d0df
Inam Ul Huq <inamulhuq44@gmail.com>
2018-06-24 21:37:26 +0500

    Git Init

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./main.sass";
+console.log("ABC");
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  stats: "minimal",
+  watch: true
+};
